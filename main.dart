543ef1378b8a87b4b2257d4a413877991a267e82
M. Meghana <56499931+Meghana846@users.noreply.github.com>
2023-12-18 12:27:01 +0530

    Add files via upload
    added background colo, a button in a row with css

---
+++ main.dart
@@ -0,0 +1,61 @@
+import 'package:calculator/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: CalculatorApp(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+
+class CalculatorApp extends StatefulWidget {
+  const CalculatorApp({Key? key});
+
+  @override
+
+  State<CalculatorApp> createState() => _CalculatorAppState();
+}
+
+class _CalculatorAppState extends State<CalculatorApp> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+          children: [
+            Expanded(
+              child: Container(color: Colors.red)),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(8),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12)),
+                          padding: const EdgeInsets.all(22),
+                          primary: buttonColor,
+                      ),
+                      onPressed: (){},
+                      child: const Text("1",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                ),
+              ),
+            ),
+          ],
+        )
+      ]
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+}
